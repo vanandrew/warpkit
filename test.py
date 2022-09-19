1cc72d9d1172bd8b079a43e76f4c436bc8fff50b
Andrew Van <vanandrew@wustl.edu>
2022-09-19 00:13:27 -0500

    :sparkles: implement pixel basis

--- test.py
+++ test.py
@@ -1,89 +1,171 @@
 from scipy.io import loadmat
+from scipy.sparse import lil_array
 import matplotlib.pyplot as plt
 import numpy as np
-from concurrent.futures import ProcessPoolExecutor
 from skimage.filters import gaussian
-
-# Load the data
-data = loadmat('phantom.mat')
-phantom = data["phantom"]
 
 
 # simulate mri imaging system
 class MRSystem:
-    def __init__(self, signal, distance=180, base_resolution=90):
+    def __init__(self, signal, distance=100, base_resolution=100, off_resonance: bool = False):
+        # store signal
         self.signal = signal
-        self.base_resolution = base_resolution
-        self.distance = distance
-        self.voxel_size = distance / base_resolution
-
-        # off resonance
-        self.off_resonance = np.zeros(signal.shape)
-        self.off_resonance[10:15, 40:50] = -1e-2
-        self.off_resonance = gaussian(self.off_resonance, sigma=(6, 12))
 
         # get signal shape
         self.shape = signal.shape
         assert len(self.shape) == 2
         assert self.shape[0] == self.shape[1]
-        self.real_resolution = self.shape[0]
-        self.real_size = distance / self.real_resolution
 
-        # compute the real resolution to base resolution ratio
-        self.resolution_ratio = self.real_resolution / self.base_resolution
+        # store distance and base resolution
+        self.distance = distance
+        self.base_resolution = base_resolution
+
+        # get voxel and physical size
+        self.voxel_size = distance / base_resolution
+        self.physical_size = distance / signal.shape[0]
+
+        # setup voxel basis
+        # get the size of the pixel relative to the original signal size
+        # this is the number of elements inside a voxel
+        self.basis_size = int(self.voxel_size / self.physical_size)
+        # check if voxel is integer multiple of physical size
+        assert np.isclose(self.basis_size, self.voxel_size / self.physical_size)
+        # check that basis size is integer multiple of signal
+        assert self.shape[0] % self.basis_size == 0
+
+        # get number of basis
+        num_basis = int(self.shape[0] / self.basis_size)
+
+        # generate the basis
+        row_index = np.zeros(((self.basis_size ** 2) * (num_basis ** 2)))
+        col_index = np.zeros(((self.basis_size ** 2) * (num_basis ** 2)))
+        data = np.ones(((self.basis_size ** 2) * (num_basis ** 2))) * (1 / (self.basis_size ** 2))
+        basis_set = lil_array((self.shape[0] * self.shape[1], num_basis * num_basis))
+        n = 0
+        resampled = np.zeros((num_basis, num_basis))
+        for j in range(num_basis):
+            for i in range(num_basis):
+                # start stop block
+                start_i = i * self.basis_size
+                end_i = (i + 1) * self.basis_size
+                start_j = j * self.basis_size
+                end_j = (j + 1) * self.basis_size
+
+                resampled[i, j] = signal[start_i:end_i, start_j:end_j].mean()
+
+                # # loop through each line of block
+                # for nj in range(start_j, end_j):
+                #     lin_start = start_i + nj * num_basis * self.basis_size
+                #     lin_end = end_i + nj * num_basis * self.basis_size
+                #     basis_set[lin_start:lin_end, n] = 1
+                # n += 1
+        self.signal = resampled
+        # self.basis = csc_array((data, (row_index, col_index)), shape=(self.shape[0], self.shape[1]))
 
         # generate position grid
-        self.y, self.x = np.mgrid[0:self.shape[0], 0:self.shape[1]]
-        self.x = self.x.astype(float) / self.shape[0]
-        self.y = self.y.astype(float) / self.shape[1]
+        self.x = np.linspace(0, distance - self.voxel_size, self.signal.shape[0])
+        self.y = np.linspace(0, distance - self.voxel_size, self.signal.shape[1])
+
+        # make grid
+        self.xx, self.yy = np.meshgrid(self.x, self.y, indexing="xy")
+
+        # setup off resonance artifact
+        self.off_resonance = np.zeros(self.signal.shape)
+        if off_resonance:
+            mid_point = self.signal.shape[0] // 2
+            self.off_resonance[-40 + mid_point - 10 : -40 + mid_point + 10, mid_point - 10 : mid_point + 10] = 2.5e-3
+            self.off_resonance = gaussian(self.off_resonance, sigma=(10, 20))
 
         # set larmor frequency
         self.larmor_frequency = 42.577 / (2 * np.pi)  # MHz / T
-        self.gamma = self.larmor_frequency
+
+        # setup gradient timings
+        self.Gx = np.zeros((self.base_resolution**2))
+        self.Gy = np.zeros((self.base_resolution**2))
+        for i in range(self.base_resolution):
+            if i % 2 == 0:
+                self.Gx[i * self.base_resolution : (i + 1) * self.base_resolution] = 1
+            else:
+                self.Gx[i * self.base_resolution : (i + 1) * self.base_resolution] = -1
+            self.Gx[i * self.base_resolution] = 0
+            if i != self.base_resolution - 1:
+                self.Gy[(i + 1) * self.base_resolution] = 1
 
         # set gradient strengths
-        self.Gx = 1 / self.larmor_frequency
-        self.Gy = 1 / self.larmor_frequency
+        self.Gx /= self.larmor_frequency
+        self.Gy /= self.larmor_frequency
 
-    def kalpha(self, gradient, duration):
-        return self.larmor_frequency * gradient * duration
+    def kalpha(self, grad_integral):
+        return self.larmor_frequency * grad_integral
 
     def kx(self, t):
-        m = np.round(t // self.base_resolution)
-        n = np.round(t % self.base_resolution)
-        if m % 2 == 0:
-            t = n
-        else:
-            t = self.base_resolution - n - 1
-        return self.kalpha(self.Gx, t)
+        grad_integral = np.sum(self.Gx[0 : t + 1])
+        return self.kalpha(grad_integral) / self.base_resolution
 
     def ky(self, t):
-        return self.kalpha(self.Gy, np.round(np.floor(t / self.base_resolution)))
+        grad_integral = np.sum(self.Gy[0 : t + 1])
+        return self.kalpha(grad_integral) / self.base_resolution
 
     def get_sample(self, t):
-        return np.sum(self.signal * np.exp(-1j * 2 * np.pi * (self.off_resonance * t + self.kx(t) * self.x + self.ky(t) * self.y)))
+        kx = self.kx(t)
+        ky = self.ky(t)
+        print("                                     ", end="\r")
+        print(f"kx: {kx:.3f}, ky: {ky:.3f}", end="\r")
+        return np.sum(
+            self.signal
+            * np.exp(-1j * 2 * np.pi * (self.off_resonance * t + kx * self.xx + ky * self.yy))
+        )
 
 
-sys = MRSystem(phantom, base_resolution=90)
-even = np.zeros(phantom.shape).astype(np.complex64)
-odd = np.zeros(phantom.shape).astype(np.complex64)
-for t in range(sys.base_resolution ** 2):
-    m = np.round(t // sys.base_resolution)
-    n = np.round(t % sys.base_resolution)
-    if m % 2 == 0:
-        even[n, m] = sys.get_sample(t)
-    else:
-        odd[sys.base_resolution - n - 1, m] = sys.get_sample(t)        
-recon = np.flipud(np.fliplr(np.fft.fft2(even + odd).T))
+def cut_kspace(k_space, cut):
+    q1 = k_space[0:cut, 0:cut]
+    q2 = k_space[0:cut, -cut:]
+    q3 = k_space[-cut:, 0:cut]
+    q4 = k_space[-cut:, -cut:]
+    h1 = np.concatenate((q1, q2), axis=1)
+    h2 = np.concatenate((q3, q4), axis=1)
+    return np.concatenate((h1, h2), axis=0)
 
-f = plt.figure()
-f.add_subplot(2, 2, 1)
-plt.imshow(np.abs(recon), cmap="gray")
-f.add_subplot(2, 2, 2)
-plt.imshow(np.angle(recon), cmap="gray")
-f.add_subplot(2, 2, 3)
-plt.imshow(np.abs(phantom), cmap="gray")
-f.add_subplot(2, 2, 4)
-plt.imshow(sys.off_resonance)
-plt.show()
 
+if __name__ == "__main__":
+    # Load the data
+    data = loadmat("phantom.mat")
+    phantom = data["phantom"]
+
+    sys = MRSystem(phantom, distance=100, base_resolution=100, off_resonance=False)
+    even = np.zeros(sys.signal.shape).astype(np.complex64)
+    odd = np.zeros(sys.signal.shape).astype(np.complex64)
+    for t in range(sys.base_resolution**2):
+        m = np.round(t // sys.base_resolution)
+        n = np.round(t % sys.base_resolution)
+        if m % 2 == 0:
+            even[m, n] = sys.get_sample(t)
+        else:
+            odd[m, sys.base_resolution - n - 1] = sys.get_sample(t)
+    print()
+    kspace = even + odd
+    recon = np.fft.ifft2(kspace)
+    orig_kspace = np.fft.fft2(phantom)
+
+    f = plt.figure()
+    f.add_subplot(2, 2, 1)
+    plt.imshow(np.log(np.abs(kspace)))
+    f.add_subplot(2, 2, 2)
+    plt.imshow(np.log(np.abs(orig_kspace)))
+    f.add_subplot(2, 2, 3)
+    plt.plot(sys.Gx)
+    plt.plot(sys.Gy)
+    f.add_subplot(2, 2, 4)
+    plt.plot(np.add.accumulate(sys.Gx))
+    plt.plot(np.add.accumulate(sys.Gy))
+
+    f = plt.figure()
+    f.add_subplot(2, 2, 1)
+    plt.imshow(np.abs(recon), cmap="gray")
+    f.add_subplot(2, 2, 2)
+    plt.imshow(np.angle(recon), cmap="gray")
+    f.add_subplot(2, 2, 3)
+    plt.imshow(np.abs(phantom), cmap="gray")
+    f.add_subplot(2, 2, 4)
+    plt.imshow(sys.off_resonance)
+    plt.show()
