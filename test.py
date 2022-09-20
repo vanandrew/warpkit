8b75e85fdaeeedd1189e26ea8fe731ab88f45a0e
Andrew Van <vanandrew@wustl.edu>
2022-09-20 03:55:08 -0500

    :construction: in-progress development

--- test.py
+++ test.py
@@ -1,13 +1,13 @@
 from scipy.io import loadmat
-from scipy.sparse import lil_array
+from scipy.sparse import dia_array
 import matplotlib.pyplot as plt
 import numpy as np
-from skimage.filters import gaussian
+from skimage.filters import gaussian, window
 
 
 # simulate mri imaging system
 class MRSystem:
-    def __init__(self, signal, distance=100, base_resolution=100, off_resonance: bool = False):
+    def __init__(self, signal, base_resolution=100, off_resonance: bool = False):
         # store signal
         self.signal = signal
 
@@ -16,55 +16,22 @@ class MRSystem:
         assert len(self.shape) == 2
         assert self.shape[0] == self.shape[1]
 
-        # store distance and base resolution
-        self.distance = distance
+        # store base resolution
         self.base_resolution = base_resolution
 
-        # get voxel and physical size
-        self.voxel_size = distance / base_resolution
-        self.physical_size = distance / signal.shape[0]
-
-        # setup voxel basis
-        # get the size of the pixel relative to the original signal size
-        # this is the number of elements inside a voxel
-        self.basis_size = int(self.voxel_size / self.physical_size)
-        # check if voxel is integer multiple of physical size
-        assert np.isclose(self.basis_size, self.voxel_size / self.physical_size)
-        # check that basis size is integer multiple of signal
-        assert self.shape[0] % self.basis_size == 0
-
-        # get number of basis
-        num_basis = int(self.shape[0] / self.basis_size)
-
         # generate the basis
-        row_index = np.zeros(((self.basis_size ** 2) * (num_basis ** 2)))
-        col_index = np.zeros(((self.basis_size ** 2) * (num_basis ** 2)))
-        data = np.ones(((self.basis_size ** 2) * (num_basis ** 2))) * (1 / (self.basis_size ** 2))
-        basis_set = lil_array((self.shape[0] * self.shape[1], num_basis * num_basis))
-        n = 0
-        resampled = np.zeros((num_basis, num_basis))
-        for j in range(num_basis):
-            for i in range(num_basis):
-                # start stop block
-                start_i = i * self.basis_size
-                end_i = (i + 1) * self.basis_size
-                start_j = j * self.basis_size
-                end_j = (j + 1) * self.basis_size
-
-                resampled[i, j] = signal[start_i:end_i, start_j:end_j].mean()
-
-                # # loop through each line of block
-                # for nj in range(start_j, end_j):
-                #     lin_start = start_i + nj * num_basis * self.basis_size
-                #     lin_end = end_i + nj * num_basis * self.basis_size
-                #     basis_set[lin_start:lin_end, n] = 1
-                # n += 1
-        self.signal = resampled
-        # self.basis = csc_array((data, (row_index, col_index)), shape=(self.shape[0], self.shape[1]))
+        # self.basis_set = np.zeros((self.shape[0] ** 2, self.shape[0] ** 2))
+        # self.system_matrix = np.zeros((self.shape[0] ** 2, self.shape[0] ** 2), dtype=np.complex64)
+        # for i in range(self.shape[0]):
+        #     for j in range(self.shape[1]):
+        #         self.basis_set[i * self.shape[0] + j, i * self.shape[0] + j] = 1
+        #         basis = np.zeros(self.shape)
+        #         basis[i, j] = 1
+        #         self.system_matrix[:, i * self.shape[0] + j] = np.fft.fft2(basis).ravel()
 
         # generate position grid
-        self.x = np.linspace(0, distance - self.voxel_size, self.signal.shape[0])
-        self.y = np.linspace(0, distance - self.voxel_size, self.signal.shape[1])
+        self.x = np.arange(self.shape[1])
+        self.y = np.arange(self.shape[0])
 
         # make grid
         self.xx, self.yy = np.meshgrid(self.x, self.y, indexing="xy")
@@ -72,52 +39,54 @@ class MRSystem:
         # setup off resonance artifact
         self.off_resonance = np.zeros(self.signal.shape)
         if off_resonance:
-            mid_point = self.signal.shape[0] // 2
-            self.off_resonance[-40 + mid_point - 10 : -40 + mid_point + 10, mid_point - 10 : mid_point + 10] = 2.5e-3
+            mid_point = self.shape[0] // 2
+            self.off_resonance[5:30, mid_point - 30 : mid_point + 30] = 1e-3
             self.off_resonance = gaussian(self.off_resonance, sigma=(10, 20))
 
         # set larmor frequency
         self.larmor_frequency = 42.577 / (2 * np.pi)  # MHz / T
 
         # setup gradient timings
-        self.Gx = np.zeros((self.base_resolution**2))
-        self.Gy = np.zeros((self.base_resolution**2))
-        for i in range(self.base_resolution):
+        self.Gx = np.zeros((self.shape[1] ** 2))
+        self.Gy = np.zeros((self.shape[0] ** 2))
+        for i in range(self.shape[1]):
             if i % 2 == 0:
-                self.Gx[i * self.base_resolution : (i + 1) * self.base_resolution] = 1
+                self.Gx[i * self.shape[1] : (i + 1) * self.shape[1]] = 1
             else:
-                self.Gx[i * self.base_resolution : (i + 1) * self.base_resolution] = -1
-            self.Gx[i * self.base_resolution] = 0
-            if i != self.base_resolution - 1:
-                self.Gy[(i + 1) * self.base_resolution] = 1
+                self.Gx[i * self.shape[1] : (i + 1) * self.shape[1]] = -1
+            self.Gx[i * self.shape[1]] = 0
+            if i != self.shape[1] - 1:
+                self.Gy[(i + 1) * self.shape[1]] = 1
 
         # set gradient strengths
         self.Gx /= self.larmor_frequency
         self.Gy /= self.larmor_frequency
+
+        self.or_term = self.off_resonance.copy()
 
     def kalpha(self, grad_integral):
         return self.larmor_frequency * grad_integral
 
     def kx(self, t):
         grad_integral = np.sum(self.Gx[0 : t + 1])
-        return self.kalpha(grad_integral) / self.base_resolution
+        return self.kalpha(grad_integral) / self.shape[1]
 
     def ky(self, t):
         grad_integral = np.sum(self.Gy[0 : t + 1])
-        return self.kalpha(grad_integral) / self.base_resolution
+        return self.kalpha(grad_integral) / self.shape[0]
+
 
     def get_sample(self, t):
         kx = self.kx(t)
         ky = self.ky(t)
         print("                                     ", end="\r")
         print(f"kx: {kx:.3f}, ky: {ky:.3f}", end="\r")
-        return np.sum(
-            self.signal
-            * np.exp(-1j * 2 * np.pi * (self.off_resonance * t + kx * self.xx + ky * self.yy))
-        )
+        res = 1 / self.shape[0]
+        self.or_term[np.round(ky / res).astype(int), np.round(kx / res).astype(int)] *= t
+        return np.sum(self.signal * np.exp(-1j * 2 * np.pi * (self.off_resonance * t + kx * self.xx + ky * self.yy)))
 
 
-def cut_kspace(k_space, cut):
+def subsample_kspace(k_space, cut):
     q1 = k_space[0:cut, 0:cut]
     q2 = k_space[0:cut, -cut:]
     q3 = k_space[-cut:, 0:cut]
@@ -127,25 +96,35 @@ def cut_kspace(k_space, cut):
     return np.concatenate((h1, h2), axis=0)
 
 
+def apply_window(data):
+    win = window("hamming", data.shape)
+    return np.fft.ifftshift(np.fft.fftshift(data) * win)
+
+
 if __name__ == "__main__":
     # Load the data
     data = loadmat("phantom.mat")
     phantom = data["phantom"]
 
-    sys = MRSystem(phantom, distance=100, base_resolution=100, off_resonance=False)
-    even = np.zeros(sys.signal.shape).astype(np.complex64)
-    odd = np.zeros(sys.signal.shape).astype(np.complex64)
-    for t in range(sys.base_resolution**2):
-        m = np.round(t // sys.base_resolution)
-        n = np.round(t % sys.base_resolution)
+    sys = MRSystem(phantom, base_resolution=100, off_resonance=True)
+    even = np.zeros(sys.shape).astype(np.complex64)
+    odd = np.zeros(sys.shape).astype(np.complex64)
+    for t in range(np.multiply.reduce(sys.shape)):
+        m = np.round(t // sys.shape[0])
+        n = np.round(t % sys.shape[1])
         if m % 2 == 0:
             even[m, n] = sys.get_sample(t)
         else:
-            odd[m, sys.base_resolution - n - 1] = sys.get_sample(t)
+            odd[m, sys.shape[1] - n - 1] = sys.get_sample(t)
     print()
     kspace = even + odd
-    recon = np.fft.ifft2(kspace)
-    orig_kspace = np.fft.fft2(phantom)
+    orig_kspace = subsample_kspace(np.fft.fft2(phantom), 5)
+
+    # apply hamming window
+    kspace_win = apply_window(kspace)
+    orig_kspace_win = apply_window(orig_kspace)
+    recon = np.fft.ifft2(kspace_win)
+    orig_recon = np.fft.ifft2(orig_kspace_win)
 
     f = plt.figure()
     f.add_subplot(2, 2, 1)
@@ -165,7 +144,7 @@ if __name__ == "__main__":
     f.add_subplot(2, 2, 2)
     plt.imshow(np.angle(recon), cmap="gray")
     f.add_subplot(2, 2, 3)
-    plt.imshow(np.abs(phantom), cmap="gray")
+    plt.imshow(np.abs(orig_recon), cmap="gray")
     f.add_subplot(2, 2, 4)
     plt.imshow(sys.off_resonance)
     plt.show()
