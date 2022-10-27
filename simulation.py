20ddb4d6e9ae73d44ddc7d6f9a133e998e804022
Andrew Van <vanandrew@wustl.edu>
2022-10-27 17:04:35 -0500

    :white_check_mark: merge fix

--- simulation.py
+++ simulation.py
@@ -1,44 +1,17 @@
 import matplotlib.pyplot as plt
 import numpy as np
-<<<<<<< HEAD
-import scipy as sp
-from scipy.io import loadmat
-=======
 from scipy.io import loadmat
 from numpy.linalg import lstsq
->>>>>>> c7e2916ec3795c9abfcf1f90ca577bf7db7cef53
 from skimage.filters import gaussian
 
 
 if __name__ == "__main__":
     # load phantom data
-<<<<<<< HEAD
-    phantom_data = loadmat("phantom.mat")
-=======
     phantom_data = loadmat("phantom3.mat")
->>>>>>> c7e2916ec3795c9abfcf1f90ca577bf7db7cef53
 
     # get the phantom
     phantom = phantom_data["phantom"]
 
-<<<<<<< HEAD
-    # create timing information for off-resonance term
-    t = np.arange(phantom.shape[0] * phantom.shape[1])
-    t = t.reshape(phantom.shape)
-
-    # EPI readout needs to be flipped for even and odd lines
-    odd_indices = np.s_[1 : phantom.shape[1] : 2]
-    t[odd_indices, :] = t[odd_indices, ::-1]
-
-    # setup off resonance artifact
-    off_resonance = np.zeros(phantom.shape)
-    mid_point = phantom.shape[0] // 2
-    off_resonance[5:30, mid_point - 30 : mid_point + 30] = 2e-3
-    off_resonance = gaussian(off_resonance, sigma=(10, 20))
-
-    # for each y line, do an fft
-    data = np.zeros(phantom.shape, dtype=np.complex64)
-=======
     # get phantom resolution
     phantom_resolution = phantom.shape[0]
 
@@ -72,31 +45,12 @@ if __name__ == "__main__":
     # for each y line, do an fft
     data = np.zeros((base_resolution, base_resolution), dtype=np.complex64)
     data_no = np.zeros((base_resolution, base_resolution), dtype=np.complex64)
->>>>>>> c7e2916ec3795c9abfcf1f90ca577bf7db7cef53
 
     # get position and k-space arrays
     pos = np.arange(phantom.shape[1])
     xx, yy = np.meshgrid(pos, pos, indexing="xy")
     k = np.arange(phantom.shape[1]) / phantom.shape[1]
 
-<<<<<<< HEAD
-    # for each x line, simulate the data collection with off-resonance term
-    for x in range(phantom.shape[1]):
-        for y in range(phantom.shape[0]):
-            print(f"Sampling Simulated K-space: {k[x]:.3f}, {k[y]:.3f}      ", end="\r")
-            data[y, x] = np.sum(phantom * np.exp(-1j * 2 * np.pi * (off_resonance * t[y, x] + k[x] * xx + k[y] * yy)))
-    print("\n")
-
-    # get image
-    img = np.fft.ifft2(data)
-
-    # plot the data
-    f = plt.figure()
-    f.add_subplot(1, 2, 1)
-    plt.imshow(np.log(np.abs(data)), cmap="gray")
-    f.add_subplot(1, 2, 2)
-    plt.imshow(np.abs(img), cmap="gray")
-=======
     # sub select k space samples that based on base resolution
     sub_range = base_resolution // 2
     selection = np.r_[0:sub_range, phantom_resolution - sub_range : phantom_resolution]
@@ -168,5 +122,4 @@ if __name__ == "__main__":
     f.add_subplot(2, 3, 6)
     plt.imshow(np.abs(off_resonance), cmap="gray")
     plt.title("Off-resonance map")
->>>>>>> c7e2916ec3795c9abfcf1f90ca577bf7db7cef53
     plt.show()
