79393c87e2160231baf58a1bbd0b7c84c7d5e8c4
Andrew Van <vanandrew77@gmail.com>
2023-10-11 15:15:28 -0500

    :arrow_up: Fix missing ROMEO dependency for user

--- warpkit/__init__.py
+++ warpkit/__init__.py
@@ -1,3 +1,23 @@
+# try to load julia ROMEO module
+import subprocess
+# ensure ROMEO is installed
+try:
+    subprocess.run(
+        [
+            "julia",
+            "-e",
+            (
+                'using Pkg; !in("ROMEO",'
+                "[dep.name for (uuid, dep) in Pkg.dependencies()])"
+                ' ? Pkg.add(Pkg.PackageSpec(;name="ROMEO", version="1.0.0")) : nothing'
+            ),
+        ],
+        check=True,
+    )
+except subprocess.CalledProcessError:
+    raise OSError("ROMEO failed to install. Check your Julia installation.")
+
+# import warpkit_cpp
 try:
     from .warpkit_cpp import *  # type: ignore
 except ImportError:
