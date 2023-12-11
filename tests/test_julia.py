8d57f9e32c01efe879c4ce3b8d8906b585b95660
Andrew Van <vanandrew77@gmail.com>
2023-12-11 03:08:12 -0600

    :white_check_mark: fix tests

--- tests/test_julia.py
+++ tests/test_julia.py
@@ -3,20 +3,5 @@ from warpkit.julia import JuliaContext
 
 
 def test_julia_context():
-    # define state variable for Context state
-    state = Manager().Value(bool, False)
-
-    # start contexts in new processes so we don't have to worry about
-    # forking a JuliaContext later on
-    def start_contexts(state):
-        Julia = JuliaContext()
-        Julia2 = JuliaContext()
-        # these two should be the same object
-        state.value = Julia is Julia2
-
-    # spawn process
-    p = Process(target=start_contexts, args=(state,))
-    p.start()
-    p.join()
     # these two should be the same object
-    assert state.value
+    assert JuliaContext() is JuliaContext()
